ca43ccf34891b344dcbc583865f7602b9fe35d33
dieverwandlung <dieverwandlung@github.com>
2020-10-08 09:09:25 +0700

    create admin dashboard pages and login page

---
+++ resources/views/admin-layout/admin-index.blade.php
@@ -0,0 +1,12 @@
+@extends('admin-layout.master-layout')
+
+@section('container')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+      <!-- Page Heading -->
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <!-- Index content here -->
+      </div>
+    </div>
+    <!-- /.container-fluid -->
+@endsection
